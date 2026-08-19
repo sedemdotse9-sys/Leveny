2d85b720bfee3946148d71e3e758154b5e022b13
Sedem Attah Dotse <sedemdotse9@gmail.com>
2026-08-19 13:44:04 +0100

    ,,,

--- js/movies.js
+++ js/movies.js
@@ -85,5 +85,6 @@ const LEVENY_MOVIES = [
     { title: "The Suicide Squad", href: "../movies/the_suicide_squad_movie.html", genre: "superhero", poster: "images/posters/the_suicide_squad.jpg", discover: 4 , background: "images/backgrounds/the_suicide_squad_backg.jpg"},
     { title: "Meg 2: The Trench", href: "../movies/meg_2_the_trench_movie.html", genre: "action", poster: "images/posters/meg_2_the_trench.jpg", discover: 1 , background: "images/backgrounds/meg_2_the_trench_backg.jpg"},
     { title: "2 Guns", href: "../movies/two_guns_movie.html", genre: "action", poster: "images/posters/2_guns.jpg", discover: 5 , background: "images/backgrounds/2_guns_backg.jpg"},
-    { title: "Now You See Me", href: "../movies/now_you_see_me_movie.html", genre: "thriller", poster: "images/posters/now_you_see_me.jpg", discover: 5 , background: "images/backgrounds/now_you_see_me_backg.jpg"}
+    { title: "Now You See Me", href: "../movies/now_you_see_me_movie.html", genre: "thriller", poster: "images/posters/now_you_see_me.jpg", discover: 5 , background: "images/backgrounds/now_you_see_me_backg.jpg"},
+    { title: "The Lost World: Jurassic Park", href: "../movies/the_lost_world_jurassic_park_movie.html", genre: "adventure", poster: "images/posters/the_lost_world_jurassic_park.jpg", discover: 3 , background: "images/backgrounds/the_lost_world_jurassic_park_backg.jpg"}
 ];
